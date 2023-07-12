69c0db6d7d393244397f0577d68b0a0f2147d3fa
Cyro Sabóia <cyrosaboia@gmail.com>
2023-07-12 17:52:24 -0300

    organizando arquivos

---
+++ questao1/MinimosQuadrados/ClassificadorLinearMSE.py
@@ -0,0 +1,83 @@
+import numpy as np
+import gzip
+import struct
+
+# Função para carregar o conjunto de dados MNIST
+def load_mnist(image_file, label_file):
+    with gzip.open(label_file, 'rb') as lbpath:
+        magic, n = struct.unpack('>II', lbpath.read(8))
+        labels = np.frombuffer(lbpath.read(), dtype=np.uint8)
+
+    with gzip.open(image_file, 'rb') as imgpath:
+        magic, num, rows, cols = struct.unpack('>IIII', imgpath.read(16))
+        images = np.frombuffer(imgpath.read(), dtype=np.uint8).reshape(len(labels), 784)
+
+    return images, labels
+
+# Conjunto de dados de treinamento
+train_images, train_labels = load_mnist('train-images-idx3-ubyte.gz', 'train-labels-idx1-ubyte.gz')
+
+# Conjunto de dados de teste
+test_images, test_labels = load_mnist('t10k-images-idx3-ubyte.gz', 't10k-labels-idx1-ubyte.gz')
+
+# print('ORIGINAL')
+# print('train_images',train_images.shape)
+# print('train_labels',train_labels.shape)
+# print('test_images',test_images.shape)
+# print('test_labels',test_labels.shape)
+
+
+Xtr = train_images.T
+Dtr = np.eye(10)[train_labels].T
+
+Xts = test_images.T
+Dts = np.eye(10)[test_labels].T
+
+# print('ONEHOT')
+# print('Xtr',Xtr.shape)
+# print('Dtr',Dtr.shape)
+# print('Xts',Xts.shape)
+# print('Dts',Dts.shape)
+
+# Parametros do classificador
+Xtr = np.vstack((-np.ones((1, Xtr.shape[1])), Xtr))  # Adiciona uma linha de -1's
+Xts = np.vstack((-np.ones((1, Xts.shape[1])), Xts))  # Adiciona uma linha de -1's
+
+# print('BIAS')
+# print('Xtr',Xtr.shape)
+# print('Xts',Xts.shape)
+
+# print('Xtr',Xtr.shape)
+# print('Dtr',Dtr.shape)
+# print('Xts',Xts.shape)
+# print('Dts',Dts.shape)
+
+# Calcular pesos
+W = np.dot(Dtr, np.dot(Xtr.T, np.linalg.pinv(np.dot(Xtr, Xtr.T))))
+
+# # Predição
+Ypred = np.dot(W, Xts)  # Saida como numeros reais
+Ypred_q = np.argmax(Ypred, axis=0)  # Encontrar a classe predita com maior valor
+
+# # print(np.argmax(Dts, axis=0)[:10])
+# # print(Ypred_q[:10])
+
+# # Taxas de acerto/erro
+Resultados = np.vstack((np.argmax(Dts, axis=0), Ypred_q))  # Saida desejada e predita lado-a-lado
+Resultados = Resultados.T  # Transpor a matriz Resultados
+Erros = Resultados[:, 0] - Resultados[:, 1]  # Coluna 1 - Coluna 2
+
+Nerros_pos = len(np.where(Erros > 0)[0])
+Nerros_neg = len(np.where(Erros < 0)[0])
+Nacertos = Xts.shape[1] - (Nerros_pos + Nerros_neg)
+
+Perros_pos = 100 * Nerros_pos / Xts.shape[1]
+Perros_neg = 100 * Nerros_neg / Xts.shape[1]
+Pacertos = 100 * Nacertos / Xts.shape[1]
+
+print("Nerros_pos:", Nerros_pos)
+print("Nerros_neg:", Nerros_neg)
+print("Nacertos:", Nacertos)
+print("Perros_pos:", Perros_pos)
+print("Perros_neg:", Perros_neg)
+print("Pacertos:", Pacertos)
---
+++ questao1/MinimosQuadrados/PCA/ClassificadorLinearMinimosQuadradosPCA.py
@@ -0,0 +1,85 @@
+import gzip
+import struct
+import numpy as np
+
+# Função para carregar o conjunto de dados MNIST
+def load_mnist(image_file, label_file):
+    with gzip.open(label_file, 'rb') as lbpath:
+        magic, n = struct.unpack('>II', lbpath.read(8))
+        labels = np.frombuffer(lbpath.read(), dtype=np.uint8)
+
+    with gzip.open(image_file, 'rb') as imgpath:
+        magic, num, rows, cols = struct.unpack('>IIII', imgpath.read(16))
+        images = np.frombuffer(imgpath.read(), dtype=np.uint8).reshape(len(labels), 784)
+
+    return images, labels
+
+# Carregar o conjunto de dados de treinamento
+train_images, train_labels = load_mnist('train-images-idx3-ubyte.gz', 'train-labels-idx1-ubyte.gz')
+
+# Carregar o conjunto de dados de teste
+test_images, test_labels = load_mnist('t10k-images-idx3-ubyte.gz', 't10k-labels-idx1-ubyte.gz')
+
+# Adicionar uma coluna de 1s para o termo de viés
+train_images = np.concatenate((train_images, np.ones((train_images.shape[0], 1))), axis=1)
+test_images = np.concatenate((test_images, np.ones((test_images.shape[0], 1))), axis=1)
+
+# Codificar os rótulos em one-hot
+num_classes = 10
+train_labels_onehot = np.eye(num_classes)[train_labels]
+test_labels_onehot = np.eye(num_classes)[test_labels]
+
+# Função para centralizar os dados
+def center_data(X):
+    X_centered = X - np.mean(X, axis=0)
+    return X_centered
+
+# Função para normalizar as características dos dados
+def normalize_data(X):
+    X_normalized = (X - np.min(X, axis=0)) / (np.max(X, axis=0) - np.min(X, axis=0) + 1e-8)
+    return X_normalized
+
+# Função para calcular o PCA
+def pca(X, num_components):
+    # Centralizar e normalizar os dados
+    X_centered = center_data(X)
+    X_normalized = normalize_data(X_centered)
+
+    # Calcular a matriz de covariância
+    cov_matrix = np.cov(X_normalized, rowvar=False)
+
+    # Calcular os autovalores e autovetores
+    eigenvalues, eigenvectors = np.linalg.eig(cov_matrix)
+
+    # Ordenar os autovalores em ordem decrescente
+    sorted_indices = np.argsort(eigenvalues)[::-1]
+    sorted_eigenvalues = eigenvalues[sorted_indices]
+    sorted_eigenvectors = eigenvectors[:, sorted_indices]
+
+    # Selecionar os principais componentes
+    selected_eigenvectors = sorted_eigenvectors[:, :num_components]
+
+    # Projetar os dados nos principais componentes
+    X_projected = np.dot(X_normalized, selected_eigenvectors)
+
+    return X_projected
+
+# Reduzir a dimensionalidade usando o PCA
+num_components = 30  # Escolha o número de componentes desejado
+train_images_reduced = pca(train_images, num_components)
+test_images_reduced = pca(test_images, num_components)
+
+# Calcular os pesos do classificador linear de mínimos quadrados
+weights = np.linalg.lstsq(train_images_reduced, train_labels_onehot, rcond=None)[0]
+
+# Realizar a classificação nos dados de teste reduzidos
+predictions = test_images_reduced @ weights
+
+# Obter os rótulos previstos como o índice do valor máximo em cada linha
+predicted_labels = np.argmax(predictions, axis=1)
+
+# Calcular a acurácia
+accuracy = np.mean(predicted_labels == test_labels)
+
+# Imprimir a acurácia
+print("Acurácia:", accuracy)
